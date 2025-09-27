cb39f1143d25eee8967e60ecc8edf3204d0eef8b
Andrea Loyola Guevara <andrea.loyola@utec.edu.pe>
2025-09-26 22:12:23 -0500

    postman y git ignore

--- app/db.py
+++ app/db.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:123@localhost:5432/policydb")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:password@db:5432/policydb")
 
 engine = create_async_engine(DATABASE_URL, future=True, echo=False)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
